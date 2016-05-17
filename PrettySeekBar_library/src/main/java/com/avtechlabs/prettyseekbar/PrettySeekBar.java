5e0e7265f6349b0230c46f44ee65e33be3511d82
v.adhithyan <aavispeaks@gmail.com>
2016-05-17 12:00:03 +0530

    unwanted files removed

--- PrettySeekBar_library/src/main/java/com/avtechlabs/prettyseekbar/PrettySeekBar.java
+++ PrettySeekBar_library/src/main/java/com/avtechlabs/prettyseekbar/PrettySeekBar.java
@@ -48,7 +48,7 @@ public class PrettySeekBar extends View{
         paint.setColor(outerCircleFillColor);
         canvas.drawCircle(viewWidthHalf, viewHeightHalf, outerCircleRadius, paint);
 
-        paint.setStyle(Paint.Style.FILL);
+        paint.setStyle(Paint.Style.STROKE);
         //paint.setColor(innerCircleFillColor);
         canvas.drawCircle(viewWidthHalf, viewHeightHalf, innerCircleRadius, paint);
     }
